2ae5dcbba36e0ce377d8b9c5e99be65eee980671
Rolan Veron Cruz <rolanvc@yahoo.com>
2017-04-15 15:32:33 +0800

    figuring out how to load images

--- fileImagetoTensor.py
+++ fileImagetoTensor.py
@@ -1,1 +1,13 @@
-class CVDataToTensors()
+import tensorflow as tf
+
+class CVDataToTensors():
+    def getimages(imagelist):
+        images = []
+        for fname in imagelist:
+            f = tf.read_file(fname)
+
+
+
+
+
+if __name__ == '__main__':
