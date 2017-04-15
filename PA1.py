cdc4f81a72c81545b83111da9499d2787190c873
Rolan Veron Cruz <rolanvc@yahoo.com>
2017-04-15 19:14:05 +0800

    network ok except for classifier

--- PA1.py
+++ PA1.py
@@ -1,27 +1,61 @@
 import crossvalidation as CV
-
 import tensorflow as tf
+import sys
 
-x = tf.placeholder(tf.int8, shape=(244, 244))
-y_ = tf.placeholder(tf.int8, shape=[7,None])
-conv1 =tf.layers.conv2d(inputs=x, filters=64, kernel_size=(7, 7), strides=(2, 2), padding='valid')
-pool1 = tf.layers.max_pooling2d(conv1, pool_size=(3, 3), strides=(2, 2), padding='valid')
-lrn1 = tf.nn.local_response_normalization(input=pool1)
-conv2a = tf.layers.conv2d(inputs=lrn1, filters=64, kernel_size=(1, 1), strides=(1, 1), padding='valid')
-pool2a = tf.layers.max_pooling2d(lrn1, pool_size=(3, 3), strides=(1, 1), padding='valid')
-conv2b = tf.layers.conv2d(inputs=conv2a, filters=64, kernel_size=(3, 3), strides=(1, 1), padding='valid')
-conv2c = tf.layers.conv2d(inputs=pool2a, filters=64, kernel_size=(1, 1), strides=(1, 1), padding='valid')
-concat2 = tf.concat([conv2b, conv2c])
-pool2b = tf.layers.max_pooling2d(inputs=concat2, pool_size=(3, 3), strides=(2, 2), padding='valid')
-conv3a = tf.layers.conv2d(inputs=pool2b, filters=64, kernel_size=(1,1), strides=(1,1), padding='valid')
-pool3a = tf.layers.max_pooling2d(inputs=pool2b, pool_size=(1, 1), strides=(3, 3), padding='valid')
-conv3b = tf.layers.conv2d(inputs=conv3a, filters=64, kernel_size=(3, 3), strides=(1, 1), padding='valid')
-conv3c = tf.layers.conv2d(inputs=pool3a, filters=64, kernel_size=(1, 1), strides=(1, 1), padding='valid')
-concat3 = tf.concat([conv3b, conv3c])
-pool3b = tf.layers.max_pooling2d(inputs=concat3, pool_size=(3, 3), strides=(2, 2), padding='valid')
+x = tf.placeholder(tf.float32, shape=(1, 224, 224, 1))
+print "x:{}".format(x)
+y_ = tf.placeholder(tf.float32, shape=[1, 7])
+print "y_:{}".format(y_)
+
+conv1 = tf.layers.conv2d(inputs=x, filters=64, kernel_size=(7, 7), strides=(2, 2), padding='same', name='conv1')
+print "conv1:{}".format(conv1)
+
+pool1 = tf.layers.max_pooling2d(conv1, pool_size=(3, 3), strides=(2, 2), padding='same', name='pool1')
+print "pool1:{}".format(pool1)
+
+lrn1 = tf.nn.local_response_normalization(input=pool1, name='lrn1')
+print "lrn1:{}".format(lrn1)
+
+conv2a = tf.layers.conv2d(inputs=lrn1, filters=96, kernel_size=(1, 1), strides=(1, 1), padding='same', name='conv2a')
+print "conv2a:{}".format(conv2a)
+
+
+conv2b = tf.layers.conv2d(inputs=conv2a, filters=208, kernel_size=(3, 3), strides=(1, 1), padding='same', name='conv2b')
+print "conv2b:{}".format(conv2b)
+
+pool2a = tf.layers.max_pooling2d(lrn1, pool_size=(3, 3), strides=(1, 1), padding='same', name='pool2a')
+print "pool2a:{}".format(pool2a)
+
+conv2c = tf.layers.conv2d(inputs=pool2a, filters=64, kernel_size=(1, 1), strides=(1, 1), padding='same', name='conv2c')
+print "conv2c:{}".format(conv2c)
+
+concat2 = tf.concat([conv2b, conv2c], axis=3, name='concat2')
+print "concat2:{}".format(concat2)
+
+pool2b = tf.layers.max_pooling2d(inputs=concat2, pool_size=(3, 3), strides=(2, 2), padding='same', name='pool2b')
+print "pool2b:{}".format(pool2b)
+
+conv3a = tf.layers.conv2d(inputs=pool2b, filters=96, kernel_size=(1, 1), strides=(1, 1), padding='same', name='conv3a')
+print "conv3a:{}".format(conv3a)
+
+pool3a = tf.layers.max_pooling2d(inputs=pool2b, pool_size=(3, 3), strides=(1, 1), padding='same', name='pool3a')
+print "pool3a:{}".format(pool3a)
+
+conv3b = tf.layers.conv2d(inputs=conv3a, filters=208, kernel_size=(3, 3), strides=(1, 1), padding='same', name='conv3b')
+print "conv3b:{}".format(conv3b)
+
+conv3c = tf.layers.conv2d(inputs=pool3a, filters=64, kernel_size=(1, 1), strides=(1, 1), padding='same', name='conv3c')
+print "conv3c:{}".format(conv3c)
+
+concat3 = tf.concat([conv3b, conv3c], axis=3, name='concat3')
+print "concat3:{}".format(concat3)
+
+pool3b = tf.layers.max_pooling2d(inputs=concat3, pool_size=(3, 3), strides=(2, 2), padding='same', name='pool3b')
+print "pool3b:{}".format(pool3b)
+
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape=shape, stdev=0.1)
+    initial = tf.truncated_normal(shape=shape,  stddev=0.1)
     return tf.Variable(initial)
 
 
@@ -29,11 +63,12 @@ def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
+y = tf.layers.dense(inputs=pool3b, units=7, name='classifier')
+print "y:{}".format(y)
 
-W = weight_variable((None, 7))
-b = bias_variable((None, 7))
-y = tf.matmul(x, W) + b
-
+y1 = tf.layers.dense(inputs=y, units=10, name='y1')
+print "y1:{}".format(y1)
+sys.exit(0)
 cv = CV.NFoldCV(10)
 sess = tf.InteractiveSession()
 
@@ -46,12 +81,12 @@ sess.run(tf.global_variables_initializer())
 for k in range(10):
     for j in range(10):
         vset, tset, vlabels, tlabels = cv.getBatch(j)
-        print "Run#{}, Fold#{}".format(k,j)
+        print "Run#{}, Fold#{}".format(k, j)
         for i in range(len(tset)):
-            train_step.run(feed_dict={x:tset[i],y:tlabels[i]})
+            train_step.run(feed_dict={x: tset[i], y: tlabels[i]})
             print conv1
             print pool1
-        print "Validation Accuracy:{}".format(accuracy.eval(feed_dict={x:vset,y:vlabels}))
+        print "Validation Accuracy:{}".format(accuracy.eval(feed_dict={x:vset, y: vlabels}))
 
 print ("Done")
 
