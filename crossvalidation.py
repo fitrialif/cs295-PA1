2488a81b11ba5bc304746f9c80cbc18306dc26c5
Rolan Veron Cruz <rolanvc@yahoo.com>
2017-04-15 12:32:41 +0800

    finished crossvalidation, starting on loading to tensors

--- crossvalidation.py
+++ crossvalidation.py
@@ -1,16 +1,34 @@
 
 import glob
 import os
+import random
 
-DATA_DIR = "/Volumes/Data/Dropbox/PhD-CS/CS 295 -Deep Learning/PA1/data/resized"
+DATA_DIR = "../data/resized"
+
+
 class NFoldCV(object):
     def __init__(self, folds):
-        self.folds = 10
+        self.numfolds = 10
         file_path = os.path.join(DATA_DIR, "*")
         self.files = glob.glob(file_path)
         self.count = len(self.files)
 
-    def getNextBatch(self):
+        random.shuffle(self.files)
+        skip = self.numfolds
+        self.folds = [self.files[i::skip] for i in xrange(self.numfolds)]
+        self.trainsets = [[file for file in self.files if file not in self.folds[j]] for j in range(len(self.folds))]
+
+
+    def showfolds(self):
+        for i in xrange(self.numfolds):
+            fold = self.folds[i]
+            trainset= self.trainsets[i]
+            print "Fold {}: length is:{}".format(i, len(fold))
+            print "Trainset {}: length is:{}".format(i, len(trainset))
+            assert(len(fold)+len(trainset) == self.count)
+
+
+    def getBatch(self,index):
         pass
 
     def getStats(self):
@@ -22,3 +40,4 @@ class NFoldCV(object):
 if __name__ == '__main__':
     cx = NFoldCV(10)
     cx.getStats()
+    cx.showfolds()
