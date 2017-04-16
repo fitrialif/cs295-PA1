f36ed1b4a97388ea2e93dc6f0db1ab9b076c7054
Rolan Veron Cruz <rolanvc@yahoo.com>
2017-04-16 10:59:10 +0800

    solved image loading with tf.expand_dims

--- fileImagetoTensor.py
+++ fileImagetoTensor.py
@@ -1,13 +1,22 @@
+from crossvalidation import NFoldCV
 import tensorflow as tf
 
 class CVDataToTensors():
-    def getimages(imagelist):
+    def getimages(self, imagelist):
         images = []
         for fname in imagelist:
-            f = tf.read_file(fname)
-
-
-
+            imagefile = tf.read_file(fname)
+            tensor = tf.image.decode_png(imagefile, channels=1)
+            resized_tensor = tf.expand_dims(tf.image.resize_images(tensor, (224, 224)), 0, name="input")
+            print "{}".format(resized_tensor)
+            images.append(resized_tensor)
+        return images
 
 
 if __name__ == '__main__':
+    u = CVDataToTensors()
+    cx = NFoldCV(10)
+    cx.getStats()
+    cx.showfolds()
+    _, tset = cx.getBatch(1)
+    images = u.getimages(tset)
